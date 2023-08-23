6deef6be5e51fa9ed9a5a0ee6f4b72043938cccd
avship <alexandershipilo@yandex.ru>
2023-08-23 14:23:29 +0300

    task2_done

---
+++ task2.js
@@ -0,0 +1,9 @@
+const mpackage = require("mpackage");
+
+const inputData = {
+  name: "макСИМ лесКин",
+  dateBirth: "10.03.2020",
+  purpose: "карьерный РОСТ",
+};
+
+console.log(mpackage(inputData));
---
+++ task2.mjs
@@ -0,0 +1,11 @@
+/*
+
+*/
+import mpackage from "mpackage";
+const inputData = {
+  name: "макСИМ лесКин",
+  dateBirth: "10.03.2020",
+  purpose: "карьерный РОСТ",
+};
+
+console.log(mpackage(inputData));
